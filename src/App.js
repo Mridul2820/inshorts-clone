149659bacb8bf799dd8406d86fa56edacb7488f1
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-11 13:31:54 +0530

    getching the api and set the news to state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import NavInshorts from './components/NavInshorts'
+import NewsContent from './components/NewsContent'
 import './styles/app.scss'
+
+const baseURL = 'http://api.mediastack.com/v1/news'
+const apiKey = `access_key=${process.env.REACT_APP_API_KEY}`
+const apiSp = 'languages=en&countries=in'
 
 const App = () => {
     const [category, setCategory] = useState("general")
+    const [newsArray, setNewsArray] = useState([])
+    // const [newsResults, setNewsResults] = useState([])
+
+    const newsAPI = async () => {
+        try {
+            const news = await axios.get(`${baseURL}?${apiKey}&categories=${category}& ${apiSp}`)
+
+            // console.log(news);
+            setNewsArray(news.data.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    console.log(newsArray);
+
+    useEffect(() => {
+        newsAPI()
+    }, [category])
+
     return (
         <div>
             <NavInshorts setCategory={setCategory} />
+            <NewsContent />
         </div>
     )
 }
---
+++ src/components/NewsContent.js
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+const NewsContent = () => {
+    return (
+        <Container maxWidth="md">
+            <h1>news</h1>
+        </Container>
+    )
+}
+
+export default NewsContent
